d1bb30d02d6ee1622390847d80086d060d9be253
sangjunpark <bobsang89@gmail.com>
2023-11-02 20:44:01 -0400

    commit

--- test/java/MyDocumentsTest.java
+++ test/java/MyDocumentsTest.java
@@ -2,7 +2,9 @@ import com.apress.isf.spring.ch02.Document;
 import com.apress.isf.spring.ch02.MySearchEngine;
 import com.apress.isf.spring.ch02.SearchEngine;
 import com.apress.isf.spring.ch02.Type;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
@@ -14,25 +16,53 @@ import static org.junit.Assert.*;
  */
 
 public class MyDocumentsTest {
-    private SearchEngine engine = new MySearchEngine();
+    //private SearchEngine engine = new MySearchEngine();
+
+    private ClassPathXmlApplicationContext context;
+    private SearchEngine engine;
+    private Type documentType;
+
+    @Before
+    public void setup(){
+        context = new ClassPathXmlApplicationContext("META-INF/mydocuments-context.xml");
+        engine = context.getBean(SearchEngine.class);
+        documentType = context.getBean(Type.class);
+    }
+//    @Test
+//    public void testFindByType(){
+//        Type documentType = new Type();
+//        documentType.setName("WEB");
+//        documentType.setDesc("Web Link");
+//        documentType.setExtension(".url");
+//
+//
+//        List<Document> documents = engine.findByType(documentType);
+//        assertNotNull(documents);
+//        assertTrue(documents.size() == 1);
+//        assertEquals(documentType.getName(), documents.get(0).getType().getName());
+//        assertEquals(documentType.getDesc(), documents.get(0).getType().getDesc());
+//        assertEquals(documentType.getExtension(), documents.get(0).getType().getExtension());
+//    }
+
+//    @Test
+//    public void testListAll(){
+//        List<Document> documents = engine.listAll();
+//        assertNotNull(documents);
+//        assertTrue(documents.size() == 4);
+//    }
+
     @Test
-    public void testFindByType(){
-        Type documentType = new Type();
-        documentType.setName("WEB");
-        documentType.setDesc("Web Link");
-        documentType.setExtension(".url");
-
-
+    public void testWithSpringFindByType(){
         List<Document> documents = engine.findByType(documentType);
+
         assertNotNull(documents);
         assertTrue(documents.size() == 1);
         assertEquals(documentType.getName(), documents.get(0).getType().getName());
         assertEquals(documentType.getDesc(), documents.get(0).getType().getDesc());
         assertEquals(documentType.getExtension(), documents.get(0).getType().getExtension());
     }
-
     @Test
-    public void testListAll(){
+    public void testWithSpringListAll(){
         List<Document> documents = engine.listAll();
         assertNotNull(documents);
         assertTrue(documents.size() == 4);
