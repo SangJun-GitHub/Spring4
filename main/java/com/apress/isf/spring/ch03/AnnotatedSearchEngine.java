91020046b3f99b0f1be3fe376ab43295f47b4af5
sangjunpark <bobsang89@gmail.com>
2023-11-03 23:21:38 -0400

    commit

---
+++ main/java/com/apress/isf/spring/ch03/AnnotatedSearchEngine.java
@@ -0,0 +1,36 @@
+package com.apress.isf.spring.ch03;
+
+import com.apress.isf.spring.ch02.Document;
+import com.apress.isf.spring.ch02.SearchEngine;
+import com.apress.isf.spring.ch02.Type;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Sang Jun Park on 11/03/2023.
+ * GitHub : http://github.com/SangJun-GitHub
+ */
+
+@Service("engine")
+public class AnnotatedSearchEngine implements SearchEngine {
+    @Autowired
+    private DocumentDAO documentDAO;
+    @Override
+    public List<Document> findByType(Type documentType) {
+        List<Document> result = new ArrayList<Document>();
+        for(Document doc : listAll()){
+            if(doc.getType().getName().equals(documentType.getName()))
+                result.add(doc);
+        }
+        return result;
+    }
+
+    @Override
+    public List<Document> listAll() {
+        return Arrays.asList(documentDAO.getAll());
+    }
+}
