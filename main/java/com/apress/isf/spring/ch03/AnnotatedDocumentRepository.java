7d4ca9ef062351572159ceb1dbc2dfb4645467fb
sangjunpark <bobsang89@gmail.com>
2023-11-03 23:26:12 -0400

    commit

---
+++ main/java/com/apress/isf/spring/ch03/AnnotatedDocumentRepository.java
@@ -0,0 +1,72 @@
+package com.apress.isf.spring.ch03;
+
+import com.apress.isf.spring.ch02.Document;
+import com.apress.isf.spring.ch02.Type;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Sang Jun Park on 11/03/2023.
+ * GitHub : http://github.com/SangJun-GitHub
+ */
+
+@Repository("documentDAO")
+public class AnnotatedDocumentRepository implements DocumentDAO{
+
+    @Override
+    public Document[] getAll() {
+        return storage();
+    }
+
+    private Document[] storage(){
+        List<Document> result = new ArrayList<Document>();
+
+        Type type = new Type();
+        type.setName("PDF");
+        type.setDesc("Portable Document Format");
+        type.setExtension(".pdf");
+
+        Document document = new Document();
+        document.setName("Book Template");
+        document.setType(type);
+        document.setLocation("/Users/felipeg/Documents/Random/Book Template.pdf");
+
+        result.add(document);
+
+        document = new Document();
+        document.setName("Sample Contract");
+        document.setType(type);
+        document.setLocation("/Users/felipeg/Documents/Contracts/Sample Contract.pdf");
+
+        result.add(document);
+
+        type = new Type();
+        type.setName("NOTE");
+        type.setDesc("Text Notes");
+        type.setExtension(".txt");
+
+        document = new Document();
+        document.setName("Clustering with RabbitMQ");
+        document.setType(type);
+        document.setLocation("/Users/felipeg/Documents/Random/Clustering with RabbitMQ.txt");
+
+        result.add(document);
+
+        type = new Type();
+        type.setName("WEB");
+        type.setDesc("Web Link");
+        type.setExtension(".url");
+
+        document = new Document();
+        document.setName("Pro Spring Security Book");
+        document.setType(type);
+        document.setLocation("http://www.apress.com/9781430248187");
+
+        result.add(document);
+
+        return result.toArray(new Document[result.size()]);
+
+    }
+}
