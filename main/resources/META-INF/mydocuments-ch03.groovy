e165b718d42a9644772100a894d1f0931be0100e
sangjunpark <bobsang89@gmail.com>
2023-11-05 23:08:29 -0500

    commit

---
+++ main/resources/META-INF/mydocuments-ch03.groovy
@@ -0,0 +1,59 @@
+import com.apress.isf.spring.ch03.SearchEngineService
+import com.apress.isf.spring.ch03.DocumentRepository
+import com.apress.isf.spring.ch02.Document
+
+beans {
+
+    engine(SearchEngineService){
+        documentDAO = ref("documentDAO")
+    }
+
+    documentDAO(DocumentRepository){
+        doc1 = ref("doc1")
+        doc2 = ref("doc2")
+        doc3 = ref("doc3")
+        doc4 = ref("doc4")
+    }
+
+    doc1(Document){
+        name = "Book Template"
+        type = ref("pdfType")
+        location = "/Users/felipeg/Documents/Random/Book Template.pdf"
+    }
+
+    doc2(Document){
+        name = "Sample Contract"
+        type = ref("pdfType")
+        location = "/Users/felipeg/Documents/Contracts/Sample Contract.pdf"
+    }
+
+    doc3(Document){
+        name = "Clustering with RabbitMQ"
+        type = ref("noteType")
+        location = "/Users/felipeg/Documents/Random/Clustering with RabbitMQ.txt"
+    }
+
+    doc4(Document){
+        name = "Pro Spring Security Book"
+        type = ref("webType")
+        location = "htttp://www.apress.com.9781430248187"
+    }
+
+    webType(com.apress.isf.spring.ch02.Type){
+        name = "WEB"
+        desc = "Web Link"
+        extension = ".url"
+    }
+
+    pdfType(com.apress.isf.spring.ch02.Type){
+        name = "PDF"
+        desc = "Portable Document Format"
+        extension = ".pdf"
+    }
+
+    noteType(com.apress.isf.spring.ch02.Type){
+        name = "NOTE"
+        desc = "Text Notes"
+        extension = ".txt"
+    }
+}
