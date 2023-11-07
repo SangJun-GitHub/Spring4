975652dbe53902083699b55d72f20cc13a70398a
sangjunpark <bobsang89@gmail.com>
2023-11-06 23:02:33 -0500

    commit

---
+++ test/java/com/apress/isf/spring/ch04/MyDocumentsTest.java
@@ -0,0 +1,45 @@
+package com.apress.isf.spring.ch04;
+
+import com.apress.isf.spring.ch02.Document;
+import com.apress.isf.spring.ch02.SearchEngine;
+import com.apress.isf.spring.ch02.Type;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+/**
+ * Created by Sang Jun Park on 11/06/2023.
+ * GitHub : http://github.com/SangJun-GitHub
+ */
+
+public class MyDocumentsTest {
+    private ClassPathXmlApplicationContext context;
+    private SearchEngine engine;
+    private Type webType;
+
+    @Before
+    public void setup(){
+        context = new ClassPathXmlApplicationContext("META-INF/mydocuments-context-ch03.xml");
+    }
+    @Test
+    public void testAll(){
+        engine = context.getBean(SearchEngine.class);
+        webType = context.getBean("webType", Type.class);
+
+        List<Document> documents = engine.findByType(webType);
+        assertNotNull(documents);
+        assertTrue(documents.size() == 1);
+        assertEquals(webType.getName(), documents.get(0).getType().getName());
+        assertEquals(webType.getDesc(), documents.get(0).getType().getDesc());
+        assertEquals(webType.getExtension(),documents.get(0).getType().getExtension());
+
+        engine = context.getBean(SearchEngine.class);
+
+        documents = engine.listAll();
+        assertNotNull(documents);
+        assertTrue(documents.size() == 4);
+    }
+}
