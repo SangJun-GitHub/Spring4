ab38ba1e4d9c7c53c6e2ad90313c15d773653055
sangjunpark <bobsang89@gmail.com>
2023-11-19 21:48:17 -0500

    commit

--- test/java/com/apress/isf/spring/ch09/MyDocumentsJDBCEmbeddedAnnotatedTest.java
+++ test/java/com/apress/isf/spring/ch09/MyDocumentsJDBCEmbeddedAnnotatedTest.java
@@ -5,47 +5,41 @@ import com.apress.isf.spring.ch09.model.Type;
 import com.apress.isf.spring.ch09.service.SearchEngine;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
- * Created by Sang Jun Park on 11/18/2023.
+ * Created by Sang Jun Park on 11/19/2023.
  * GitHub : http://github.com/SangJun-GitHub
  */
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:META-INF/ch09/mydocuments-jdbc-context-ch09.xml")
-public class MyDocumentJDBCTest {
-    private static final Logger log = LoggerFactory.getLogger(MyDocumentJDBCTest.class);
+@ContextConfiguration("classpath:META-INF/ch09/mydocuments-jdbc-embedded-context-ch09.xml")
+public class MyDocumentsJDBCEmbeddedAnnotatedTest {
 
     @Autowired
     private SearchEngine engine;
+    //error
 
     private Type webType = new Type("WEB", ".url");
 
     @Test
     public void testUsingSpringJDBC(){
-        log.debug("Using Spring JDBC...");
 
         List<Document> documents = engine.listAll();
         assertNotNull(documents);
         assertTrue(documents.size() == 4);
 
-        //error type is not injected into documents
+        documents = engine.findByType(webType);
+        assertNotNull(documents);
+        assertTrue(documents.size() == 1);
+        assertEquals(webType.getName(), documents.get(0).getType().getName());
+        assertEquals(webType.getDesc(), documents.get(0).getType().getDesc());
+        assertEquals(webType.getExtension(), documents.get(0).getType().getExtension());
 
-//        documents = engine.findByType(webType);
-//        assertNotNull(documents);
-//        assertTrue(documents.size() == 1);
-//        assertEquals(webType.getName(), documents.get(0).getType().getName());
-//        assertEquals(webType.getDesc(), documents.get(0).getType().getDesc());
-//        assertEquals(webType.getExtension(), documents.get(0).getType().getExtension());
-//
-//        log.debug("Found WEB Document: " + documents.get(0));
     }
 }
