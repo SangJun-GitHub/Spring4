d2b956050149bf37531835d135f2a8400c5c2006
Sang Jun Park <bobsang89@gmail.com>
2023-11-28 23:32:18 -0500

    commit

---
+++ main/java/com/apress/isf/spring/ch14/java/service/DocumentService.java
@@ -0,0 +1,20 @@
+package com.apress.isf.spring.ch14.java.service;
+
+import com.apress.isf.spring.ch14.java.model.Document;
+import com.apress.isf.spring.ch14.java.model.Type;
+
+import java.util.List;
+
+/**
+ * Created by Sang Jun Park on 11/28/2023.
+ * GitHub : http://github.com/SangJun-GitHub
+ */
+
+public interface DocumentService {
+    public List<Document> getAllDocuments();
+    public Document findDocumentById(String id);
+    public Document saveDocument(String id, Document document);
+    public Document removeDocument(String id);
+    public List<Document> findByType(Type type);
+    public boolean updateLocationFromDocumentId(String documentId, String location);
+}
---
+++ main/java/com/apress/isf/spring/ch14/spring/service/DocumentServiceFacade.java
@@ -0,0 +1,9 @@
+package com.apress.isf.spring.ch14.spring.service;
+
+/**
+ * Created by Sang Jun Park on 11/28/2023.
+ * GitHub : http://github.com/SangJun-GitHub
+ */
+
+public class DocumentServiceFacade {
+}
