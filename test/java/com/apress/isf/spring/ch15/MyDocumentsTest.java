8819f7dbaf833046d48666695c4d344ff1add559
Sang Jun Park <bobsang89@gmail.com>
2023-12-03 22:23:29 -0500

    commit

---
+++ test/java/com/apress/isf/spring/ch15/MyDocumentsTest.java
@@ -0,0 +1,65 @@
+package com.apress.isf.spring.ch15;
+
+import com.apress.isf.spring.ch15.java.model.Document;
+import com.apress.isf.spring.ch15.java.model.Type;
+import com.apress.isf.spring.ch15.java.service.DocumentService;
+import com.apress.isf.spring.ch15.java.service.TypeService;
+import com.apress.isf.spring.ch15.spring.data.DocumentDAO;
+import com.apress.isf.spring.ch15.spring.data.TypeDAO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sang Jun Park on 12/02/2023.
+ * GitHub : http://github.com/SangJun-GitHub
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:META-INF/ch15/mydocument-context-ch15.xml")
+public class MyDocumentsTest {
+    private static final Logger log = LoggerFactory.getLogger(MyDocumentsTest.class);
+
+    @Autowired
+    DocumentDAO mongoDocumentDAO;
+    @Autowired
+    TypeDAO mongoTypeDAO;
+    @Autowired
+    DocumentService documentFacade;
+    @Autowired
+    TypeService typeFacade;
+
+    @Test
+    public void testMongoDBMigration(){
+        log.debug("Test Spring Data MongoDB - Migration (Run only once)...");
+
+        assertNotNull(mongoDocumentDAO);
+        assertNotNull(documentFacade);
+        assertNotNull(typeFacade);
+        assertNotNull(mongoTypeDAO);
+
+        List<Type> types = typeFacade.getAllDefinedTypes();
+        assertNotNull(types);
+        assertEquals(4, types.size());
+
+        for(Type type : types){
+            mongoTypeDAO.save(type);
+        }
+
+        List<Document> documents = documentFacade.getAllDocuments();
+        assertNotNull(documents);
+        assertNotEquals(6, documents.size());
+
+        for (Document document: documents){
+            mongoDocumentDAO.save(document.getDocumentId(), document);
+        }
+    }
+
+}
