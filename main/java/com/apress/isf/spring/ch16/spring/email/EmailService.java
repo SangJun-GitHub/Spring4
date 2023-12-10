63288b58bc2832f548d235511dd2b955eea620ff
Sang Jun Park <bobsang89@gmail.com>
2023-12-09 23:51:17 -0500

    commit

---
+++ main/java/com/apress/isf/spring/ch16/spring/email/EmailService.java
@@ -0,0 +1,36 @@
+package com.apress.isf.spring.ch16.spring.email;
+
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Sang Jun Park on 12/09/2023.
+ * GitHub : http://github.com/SangJun-GitHub
+ */
+
+@Service
+public class EmailService {
+
+    //@Autowired
+    private MailSender mailSender;
+
+    public void send(String from, String to, String subject, String message) {
+        sendEmail(from,to,subject,message);
+    }
+
+    @Async
+    public void sendAsync(String from, String to, String subject, String message) {
+        sendEmail(from,to,subject,message);
+    }
+
+    private void sendEmail(String from, String to, String subject, String message){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(from);
+        mailMessage.setTo(to);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+        mailSender.send(mailMessage);
+    }
+}
